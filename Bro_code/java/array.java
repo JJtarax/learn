ad7a4bf6a4b9e02e4c7e1ab4d439814a87e960d2
JJtarax <93460521+JJtarax@users.noreply.github.com>
2023-07-13 23:11:11 -0500

    wasd

--- Bro_code/java/array.java
+++ Bro_code/java/array.java
@@ -3,7 +3,16 @@ package learn.Bro_code.java;
 public class array {
 
     public static void main(String[] args) {
-        Scanner scanner =
+        
+        String[] cars = new String[3];
+
+        cars[0] = "Camaro";
+        cars[1] = "Corvette";
+        cars[2] = "Tesla";
+
+        for(int i=0; i<cars.length; i++) {
+            System.out.println(cars[i]);
+        }
     }
 
 }
