38bf6e82fe1af487b0e5731c7dca364bdd74fd9a
JJtarax <93460521+JJtarax@users.noreply.github.com>
2023-07-21 20:58:06 -0400

    update

---
+++ Bro_code/java/array/_2d_array_list.java
@@ -0,0 +1,21 @@
+package array;
+
+import java.util.*;
+
+public class _2d_array_list {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> bakeryList = new ArrayList();
+        bakeryList.add("pasta");
+        bakeryList.add("garlic bread");
+        bakeryList.add("donuts");
+
+        ArrayList<String> produceList = new ArrayList();
+        produceList.add("pasta");
+        bakeryList.add("garlic bread");
+        bakeryList.add("donuts");
+
+        System.out.println(bakeryList.get(0));
+    }
+}
