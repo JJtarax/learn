a9192895b7c66abde781cd8bf3d590d93cd5186b
JJtarax <93460521+JJtarax@users.noreply.github.com>
2023-08-30 10:42:03 -0400

    wasd

--- Bro_code/java/array/_2d_array.java
+++ Bro_code/java/array/_2d_array.java
@@ -28,7 +28,7 @@ public class _2d_array {
         for (int i = 0; i < cars.length; i++) {
             System.out.println();
             for (int j = 0; j < cars[i].length; j++) {
-                System.out.print(cars[i][j] + " ");
+                System.out.print(cars[i][j] + "|");
             }
         }
     }
